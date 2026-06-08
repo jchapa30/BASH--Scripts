d3951686b7a8aff65f5bbedd26bba7dcac37ecae
Joey Chapa <joeychapa@yahoo.com>
2026-06-08 10:08:13 -0500

    Remove scripts from repository root

--- file.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ -f ~/myfile.sh ]
-then
- echo "File exists"
-else
- echo :" File does not exist"
-fi
--- if_statements.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-my_number=200
-
-
-if [ $my_number -eq 200 ]
-then
- echo " The condition is true"
-fi
